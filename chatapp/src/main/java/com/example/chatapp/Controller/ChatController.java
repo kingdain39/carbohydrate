0e92900fbe8d7626d5c4e283d36600c17ac9288d
kingdain39 <dain39@ewha.ac.kr>
2025-11-28 18:36:15 +0900

    컨트롤러전반수정

--- chatapp/src/main/java/com/example/chatapp/Controller/ChatController.java
+++ chatapp/src/main/java/com/example/chatapp/Controller/ChatController.java
@@ -2,29 +2,17 @@
 @RequiredArgsConstructor
 public class ChatController{
 
-    private final MessageService messageService;
+    private final ChatService chatService;
     private final UserService userService;
     private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/chat.send")
     public void sendMessage(@Payload ChatSendRequest request){
-        User sender = userService.findById(request.getSenderId());
+        User sender = userService.findById(request.getSenderId()); //UserService 파일따로 만들어 둿길래 그냥 일케 둘게
 
-        Message message = messageService.saveMessage(
-                sender,
-                request.getContent(),
-                null
-        );
-
-
-        ChatMessageResponse response = ChatMessageResponse.builder()
-                .type("CHAT")
-                .senderName(sender.getUserName())
-                .content(request.getContent())
-                .timestamp(LocalDateTime.now())
-                .build();
-
-        messagingTemplate.convertAndSend("/topic/public",response);
+        //빌드하는거 싹 삭제하고 서비스 불러주미
+        ChatMessageResponse response = chatService.savePublicMessage(request);//requst dto그대로 걍 넘겨줌.
+        messagingTemplate.convertAndSend("/topic/public",response); //이거가 응답을 /topic/public을 구독중인 모두에게 브로드캐스팅하는거
     }
 
 
@@ -33,28 +21,13 @@ public class ChatController{
         User sender = userService.findById(request.getSenderId());
         User recipient = userService.findById(request.getRecipientId());
 
-
-        Message message = messageService.saveMessage(
-                sender,
-                request.getContent(),
-                recipient
-        );
-
-        ChatMessageResponse response = ChatMessageResponse.builder()
-                .type("WHISPER")
-                .senderName(sender.getUserName())
-                .content(request.getContent())
-                .timestamp(LocalDateTime.now())
-                .build();
-
-
-        messagingTemplate.convertAndSendToUser(
-                sender.getUserName(),
+        messagingTemplate.convertAndSendToUser( // 특정유저의 /queue/whisper에다가만 브로드캐슽ㅇ
+                sender.getUserName(),  //이거는 자기가 남에게 보낸 귓속말 메세지
                 "/queue/whisper",
                 response
         );
         messagingTemplate.convertAndSendToUser(
-                recipient.getUserName(),
+                recipient.getUserName(), //이건 수신자용 남으로부터 받은 귓속말메세지띄워주기
                 "/queue/whisper",
                 response
         );
@@ -64,18 +37,7 @@ public class ChatController{
     public void joinChat(@Payload JoinRequest request,
                          SimpMessageHeaderAccessor headerAccessor){
         User user = userService.findById(request.getUserId());
-
-
-        headerAccessor.getSessionAttributes().put("userId", user.getId());
-        headerAccessor.getSessionAttributes().put("userName", user.getUserName());
-
-        userService.addActiveUser(user);
-        SystemMessageResponse response = SystemMessageResponse.builder()
-                .type("JOIN")
-                .content(user.getUserName() + "님이 입장하셨습니다.")
-                .timestamp(LocalDateTime.now())
-                .build();
-
+        ChatMessageResponse response = chatService.handleJoin(request, headerAccessor);
         messagingTemplate.convertAndSend("/topic/user",response);
     }
 
