190a47f5787c50868c8b2155b3e1069f9fbdc845
kingdain39 <dain39@ewha.ac.kr>
2025-11-28 23:45:04 +0900

    병합완료

--- chatapp/src/main/java/kr/co/carbohydrate/chat/controller/ChatController.java
+++ chatapp/src/main/java/kr/co/carbohydrate/chat/controller/ChatController.java
@@ -1,99 +1,57 @@
 package kr.co.carbohydrate.chat.controller;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import org.apache.logging.log4j.message.Message;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.stereotype.Controller;
-
 import kr.co.carbohydrate.chat.dto.ChatMessageResponse;
 import kr.co.carbohydrate.chat.dto.ChatSendRequest;
 import kr.co.carbohydrate.chat.dto.JoinRequest;
-import kr.co.carbohydrate.chat.entity.ChatUserEntity;
+import kr.co.carbohydrate.chat.dto.WhisperRequest;
 import kr.co.carbohydrate.chat.service.ChatService;
-import kr.co.carbohydrate.chat.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
 public class ChatController{
 
-    private ChatService chatService;
-    private UserService userService;
-    private SimpMessageSendingOperations messagingTemplate;
+    private final ChatService chatService;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    
     @MessageMapping("/chat.send")
     public void sendMessage(@Payload ChatSendRequest request){
-    	ChatUserEntity sender = userService.findById(request.getSenderId());
-
-        Message message = chatService.saveMessage(
-                sender,
-                request.getContent(),
-                null
-        );
-
-
-        ChatMessageResponse response = ChatMessageResponse.builder()
-                .type("CHAT")
-                .senderName(sender.getUserName())
-                .content(request.getContent())
-                .timestamp(LocalDateTime.now())
-                .build();
-
-        messagingTemplate.converAndSend("/topic/public",response);
+        //빌드하는거 싹 삭제하고 서비스 불러주미
+        ChatMessageResponse response = chatService.savePublicMessage(request);//requst dto그대로 걍 넘겨줌.
+        messagingTemplate.convertAndSend("/topic/public",response); //이거가 응답을 /topic/public을 구독중인 모두에게 브로드캐스팅하는거
     }
 
 
     @MessageMapping("/chat.whisper")
     public void sendWhisper(@Payload WhisperRequest request) {
-        ChatUser sender = userService.findById(request.getSenderId());
-        ChatUser recipient = userService.findById(request.getRecipieintId());
+        ChatMessageResponse response = chatService.saveWhisperMessage(request);
 
-
-        Message message = messageService.saveMesseage(
-                sender,
-                request.getContent(),
-                recipient
-        );
-
-        ChatMessageResponse response = ChatMessageResponse.builder()
-                .type("WHISPER")
-                .senderName(sender.getUserName())
-                .content(request.getContent())
-                .timestamp(LocalDateTime.now())
-                .build();
-
-
-        messagingTemplate.convertAndSendToUser(
-                sender.getUserName(),
+        messagingTemplate.convertAndSendToUser( // 특정유저의 /queue/whisper에다가만 브로드캐슽ㅇ
+                response.getSenderName(),  //이거는 자기가 남에게 보낸 귓속말 메세지
                 "/queue/whisper",
                 response
         );
         messagingTemplate.convertAndSendToUser(
-                recipient.getUserName(),
+                response.getRecipientName(), //이건 수신자용 남으로부터 받은 귓속말메세지띄워주기
                 "/queue/whisper",
                 response
         );
     }
-    //입장처리 클라이언트가 /app/chat.join으로 메시지를 보내면 실행됨
+
     @MessageMapping("/chat.join")
     public void joinChat(@Payload JoinRequest request,
-    		SimpMessageHeaderAccessor headerAccessor){
-    	// 1. 유저 정보 조회
-        ChatUserEntity user = userService.findById(request.getUserId());
-        // 2. 웹소켓 세션에 사용자 이름 저장! (퇴장 감지용)
-        // ★ 중요: 여기서 저장한 "userName"을 나중에 Listener에서 꺼내 씁니다.
-        headerAccessor.getSessionAttributes().put("userName", user.getUserName());
-        //3.서비스 호출
-        chatService.userJoin(user.getUserName());
+                         SimpMessageHeaderAccessor headerAccessor){
+        ChatMessageResponse response = chatService.handleJoin(request, headerAccessor);
+        messagingTemplate.convertAndSend("/topic/user",response);
     }
-    
-    
+
+
+
 
 
 }
--- chatapp/src/main/java/kr/co/carbohydrate/chat/dto/ChatSendRequest.java
+++ chatapp/src/main/java/kr/co/carbohydrate/chat/dto/ChatSendRequest.java
@@ -1,10 +1,12 @@
+package kr.co.carbohydrate.chat.dto;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Getter
-@NoArgsConstructor @AllArgsContructor
+@NoArgsConstructor @AllArgsConstructor
 public class ChatSendRequest {
     private Long senderId;
     private String content;
