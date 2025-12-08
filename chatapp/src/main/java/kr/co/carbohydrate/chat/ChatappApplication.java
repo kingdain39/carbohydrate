1cce6c49ca3c6415fdf3c9baa7375db1f1eca5f3
yerim seo <bmo2683@naver.com>
2025-12-08 14:53:37 +0900

    feature: 스프링 부트 시작 메시지 출력

--- chatapp/src/main/java/kr/co/carbohydrate/chat/ChatappApplication.java
+++ chatapp/src/main/java/kr/co/carbohydrate/chat/ChatappApplication.java
@@ -8,6 +8,7 @@ public class ChatappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatappApplication.class, args);
+		System.out.println("귓속말 채팅 프로그램 시작!");
 	}
 
 }
